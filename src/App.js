242d920b3570a28eb68e515f1a670dbef525f332
Sarah Bentley <sarahbentley@MacBook-Pro-6.local>
2024-07-08 22:14:37 +0200

    new site big updates

--- src/App.js
+++ src/App.js
@@ -1,44 +1,45 @@
-import React, {Component} from "react"
 import './App.css';
-import NavBar from './Components/NavBar.js'
-import Header from './Components/Header.js'
-import About from './Components/About.js'
-import Experience from './Components/Experience.js'
-import Work from './Components/Work.js'
-import Footer from './Components/Footer.js'
+import NavBar from './Components/js/NavBar.js'
+import Home from './Components/js/Home.js'
+import About from './Components/js/About.js'
+import Experience from './Components/js/Experience.js'
+import Footer from './Components/js/Footer.js'
+import Math from './Components/js/Math.js'
+import Lifestyle from './Components/js/Lifestyle.js'
+// import LifestylePost from './Components/js/LifestylePost.js'
+// import MathPost from './Components/js/MathPost.js'
 
 import {
-  HashRouter as Router,
+  BrowserRouter as Router,
   Switch,
   Route
 } from "react-router-dom";
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
-    <>
+export default function App() {
+  return (
     <Router>
       <div className='App-container'>
-        <NavBar />
-          <div className="Switch-container">
+        {/* Conditional render NavBar based on the route */}
+        <Route
+          render={({ location }) =>
+            location.pathname !== '/' && <NavBar />
+          }
+        />
+        
+        <div className="Switch-container">
           <Switch>
-            <Route exact component={Header} path="/" />
+            <Route exact component={Home} path="/" />
             <Route exact component={About} path="/about" />
             <Route exact component={Experience} path="/experience" />
-            <Route exactly component={Work} pattern="/work" />
+            <Route exact component={Math} path="/math_cs" />
+            <Route exact component={Lifestyle} path="/lifestyle" />
+            {/* <Route component={LifestylePost} path="/lifestyle/:id" /> */}
+            {/* <Route component={MathPost} path="/math_cs/:id" /> Dynamic route for blog posts */}
           </Switch>
-          </div>
-          <Footer/>
+        </div>
+        
+        <Footer />
       </div>
-      </Router>
-    </>
-    
-    )
+    </Router>
+  );
 }
-}
-
-export default App;
