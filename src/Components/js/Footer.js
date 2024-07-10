958dcf3814a5ea250bc63c61ea3fa7f029431cde
Sarah Bentley <sarahbentley@tsf-436-wpa-3-175.epfl.ch>
2024-07-10 10:49:03 +0200

    small footer change

--- src/Components/js/Footer.js
+++ src/Components/js/Footer.js
@@ -7,9 +7,6 @@ const Footer = () => {
         return (
             <footer>
                 <div className="Footer-linkContainer">
-                    <h3>
-                        sbentley@mit.edu
-                    </h3>
                     <a href="https://www.linkedin.com/in/sarah-bentley-2722a51ab/"><img alt="" className="Footer-img" src="linkedin_logo.png"></img></a>
                 </div>
             </footer>
