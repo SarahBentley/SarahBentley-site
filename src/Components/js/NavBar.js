db9d75a7a86a1072639ac80f1234cba0d83a11cc
Sarah Bentley <sarahbentley@dhcp-10-29-136-187.dyn.MIT.EDU>
2024-11-26 14:02:03 -0500

    making mobile compatible

--- src/Components/js/NavBar.js
+++ src/Components/js/NavBar.js
@@ -1,37 +1,49 @@
-import '../../utilities.css'
+import { React, useState } from 'react';
+import { NavLink } from 'react-router-dom';
+import '../../utilities.css';
 import "../css/NavBar.css";
-import { React} from 'react';
-import {
-    NavLink
-  } from "react-router-dom";
 
 const NavBar = () => {
-        return (
-            <nav className="NavBar-container">
-                <div className="wrap">
-                    <li className="NavBar-logo old">
-                        <NavLink style={{ textDecoration: 'none' }} className = "NavBar-link" to="/">SB</NavLink>
-                    </li>
-                    <li className="NavBar-logo new">
-                        <NavLink style={{ textDecoration: 'none' }} className = "NavBar-link" to="/">SARAH BENTLEY</NavLink>
-                    </li>
-                    <div className="NavBar-linkContainer">
-                        <li><h4>
-                            <NavLink style={{ textDecoration: 'none' }} className = "NavBar-link" activeClassName="NavBar-link-active" to="/about">About me</NavLink>
-                        </h4></li>
-                        <li><h4>
-                            <NavLink style={{ textDecoration: 'none' }} className = "NavBar-link" activeClassName="NavBar-link-active" to="/experience">Experience</NavLink>
-                        </h4></li>
-                        <li><h4>
-                            <NavLink style={{ textDecoration: 'none' }} className = "NavBar-link" activeClassName="NavBar-link-active" to="/math_cs">Math&CS</NavLink>
-                        </h4></li>
-                        <li><h4>
-                            <NavLink style={{ textDecoration: 'none' }} className = "NavBar-link" activeClassName="NavBar-link-active" to="/lifestyle">Lifestyle</NavLink>
-                        </h4></li>
-                    </div>
-                </div>
-            </nav>
-        )
-}
+    const [isMenuOpen, setMenuOpen] = useState(false);
 
-export default NavBar
+    const toggleMenu = () => {
+        setMenuOpen(!isMenuOpen);
+    };
+
+    return (
+        <nav className="NavBar-container">
+            <div className="wrap">
+                <li className="NavBar-logo old">
+                    <NavLink style={{ textDecoration: 'none' }} className="NavBar-link" to="/">SB</NavLink>
+                </li>
+                <li className="NavBar-logo new">
+                    <NavLink style={{ textDecoration: 'none' }} className="NavBar-link" to="/">SARAH BENTLEY</NavLink>
+                </li>
+                <button 
+                    className="hamburger-menu" 
+                    onClick={toggleMenu} 
+                    aria-label="Toggle menu">
+                    <div className={`line ${isMenuOpen ? 'open' : ''}`}></div>
+                    <div className={`line ${isMenuOpen ? 'open' : ''}`}></div>
+                    <div className={`line ${isMenuOpen ? 'open' : ''}`}></div>
+                </button>
+                <div className={`NavBar-linkContainer ${isMenuOpen ? 'show' : ''}`}>
+                    <li><h4>
+                        <NavLink style={{ textDecoration: 'none' }} className="NavBar-link" activeClassName="NavBar-link-active" to="/about">About me</NavLink>
+                    </h4></li>
+                    <li><h4>
+                        <NavLink style={{ textDecoration: 'none' }} className="NavBar-link" activeClassName="NavBar-link-active" to="/experience">Experience</NavLink>
+                    </h4></li>
+                    <li><h4>
+                        <NavLink style={{ textDecoration: 'none' }} className="NavBar-link" activeClassName="NavBar-link-active" to="/math_cs">Math&CS</NavLink>
+                    </h4></li>
+                    <li><h4>
+                        <NavLink style={{ textDecoration: 'none' }} className="NavBar-link" activeClassName="NavBar-link-active" to="/lifestyle">Lifestyle</NavLink>
+                    </h4></li>
+                </div>
+            </div>
+        </nav>
+    );
+};
+
+export default NavBar;
